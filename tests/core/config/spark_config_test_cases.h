396132aa46c270d8755107146bc20ab2172cfd5f
ReallocAll <63277109+ReallocAll@users.noreply.github.com>
2026-08-24 15:44:36 +0800

    refactor: split spark config tests

---
+++ tests/core/config/spark_config_test_cases.h
@@ -0,0 +1,23 @@
+#ifndef SPARK_TESTS_CORE_CONFIG_SPARK_CONFIG_TEST_CASES_H
+#define SPARK_TESTS_CORE_CONFIG_SPARK_CONFIG_TEST_CASES_H
+
+namespace spark::config_test {
+
+void testDefaults();
+void testTomlValidOverride();
+void testTomlInvalid();
+void testTomlWrongType();
+void testBootstrapPreservesInvalidFile();
+void testValidation();
+void testEndpointValidation();
+void testTomlUnknownField();
+void testTomlPartial();
+void testSaveCreatesToml();
+void testSaveAndReload();
+void testEmptyToml();
+void testTomlWithComments();
+void testBoundedAndTrailingInput();
+
+}  // namespace spark::config_test
+
+#endif  // SPARK_TESTS_CORE_CONFIG_SPARK_CONFIG_TEST_CASES_H
